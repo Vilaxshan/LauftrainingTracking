7b13899e8a98419700190c373c3a2028c2d32186
Vilaxshan Vinayaganathan <Vilaxshan.Vinayaganathan@netcetera.com>
2022-01-17 09:17:58 +0100

    Replaced button on this page with "Start" button

--- app/src/main/java/com/example/lauftrainingtracking/MainActivity.java
+++ app/src/main/java/com/example/lauftrainingtracking/MainActivity.java
@@ -1,45 +1,26 @@
 package com.example.lauftrainingtracking;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.NumberPicker;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button back;
-    Button begin;
-
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        back = findViewById(R.id.button_back);
-        begin = findViewById(R.id.button_begin);
+        start = findViewById(R.id.start);
 
-
-        begin.setOnClickListener(new View.OnClickListener() {
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setContentView(R.layout.activity_counter);
-            }
-        });
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_start);
             }
         });
     }
