cbcc4d216be6c71d574694f9e001ec37fe5287b1
Vilaxshan Vinayaganathan <Vilaxshan.Vinayaganathan@netcetera.com>
2022-01-17 09:19:26 +0100

    Implemented Notification functionality

--- app/src/main/java/com/example/lauftrainingtracking/CounterActivity.java
+++ app/src/main/java/com/example/lauftrainingtracking/CounterActivity.java
@@ -1,10 +1,13 @@
 package com.example.lauftrainingtracking;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class CounterActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -26,29 +31,31 @@ public class CounterActivity extends AppCompatActivity implements SensorEventLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_main);
 
         finish = findViewById(R.id.button_finish);
         steps = findViewById(R.id.steps);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
 
-
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.activity_start);
+                setContentView(R.layout.activity_main);
+
+                notificationChannel();
+                createNotification();
             }
         });
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         run = true;
         Sensor count = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
-        if (count !=null) {
+        if (count != null) {
             sensorManager.registerListener(this, count, SensorManager.SENSOR_DELAY_UI);
         } else {
             Toast.makeText(this, "Es wurde kein Schritt-Sensor auf ihrem Smartphone erkannt.", Toast.LENGTH_SHORT).show();
@@ -75,5 +82,34 @@ public class CounterActivity extends AppCompatActivity implements SensorEventLis
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    private static final String CHANNEL_ID = "chanel1";
+
+    private void notificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void createNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("")
+                .setContentText("")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(""))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(1, builder.build());
     }
 }
