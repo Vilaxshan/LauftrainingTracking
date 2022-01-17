e2aa60e19fd2c06ee59e3a2fd4e4331c614ff161
Vilaxshan Vinayaganathan <Vilaxshan.Vinayaganathan@netcetera.com>
2022-01-17 09:04:29 +0100

    Implemented step sensor for counting the steps

--- app/src/main/java/com/example/lauftrainingtracking/CounterActivity.java
+++ app/src/main/java/com/example/lauftrainingtracking/CounterActivity.java
@@ -1,16 +1,27 @@
 package com.example.lauftrainingtracking;
 
-
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CounterActivity extends AppCompatActivity {
+public class CounterActivity extends AppCompatActivity implements SensorEventListener {
 
     Button finish;
+
+    TextView steps;
+
+    SensorManager sensorManager;
+
+    boolean run = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,9 @@ public class CounterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         finish = findViewById(R.id.button_finish);
+        steps = findViewById(R.id.steps);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
 
 
         finish.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +40,40 @@ public class CounterActivity extends AppCompatActivity {
                 setContentView(R.layout.activity_start);
             }
         });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        run = true;
+        Sensor count = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        if (count !=null) {
+            sensorManager.registerListener(this, count, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Es wurde kein Schritt-Sensor auf ihrem Smartphone erkannt.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        run = false;
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        if (run) {
+            steps.setText(String.valueOf(sensorEvent.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
